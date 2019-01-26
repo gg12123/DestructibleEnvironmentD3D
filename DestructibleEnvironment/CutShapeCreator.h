b4fed695eb7c42ccf5e3f7e602d3e5646f944e2b
George Godfrey <georgegodfrey24@gmail.com>
2019-01-26 11:58:56 +0000

    Work on cut shape creator.

--- DestructibleEnvironment/CutShapeCreator.h
+++ DestructibleEnvironment/CutShapeCreator.h
@@ -12,43 +12,141 @@
 class CutShapeCreator
 {
 private:
+	class ClosestPlanes
+	{
+	private:
+		bool AlreadyGotPlane(int id, int& index) const
+		{
+			for (auto i = 0; i < MaxNumPlanes; i++)
+			{
+				if (m_PlanesIds[i] == id)
+				{
+					index = i;
+					return true;
+				}
+			}
+			return false;
+		}
+
+	public:
+		static constexpr int MaxNumPlanes = 3;
+
+		void Reset()
+		{
+			for (int i = 0; i < MaxNumPlanes; i++)
+			{
+				m_Distances[i] = MathU::Infinity;
+				m_PlanesIds[i] = -1;
+			}
+			m_WithinTolPlanes.clear();
+		}
+
+		void Update(int id, const Plane& plane, float dist)
+		{
+			int indexOfExisting;
+			if (AlreadyGotPlane(id, indexOfExisting))
+			{
+				if (m_Distances[indexOfExisting] < dist)
+				{
+					m_Distances[indexOfExisting] = dist;
+					m_Planes[indexOfExisting] = plane;
+				}
+				return;
+			}
+
+			for (int i = 0; i < MaxNumPlanes; i++)
+			{
+				if (m_Distances[i] < dist)
+				{
+					m_Distances[i] = dist;
+					m_Planes[i] = plane;
+					m_PlanesIds[i] = id;
+					return;
+				}
+			}
+		}
+
+		void FinishUpdating(float tol)
+		{
+			Plane closest;
+			auto closestDist = MathU::Infinity;
+
+			for (int i = 0; i < MaxNumPlanes; i++)
+			{
+				auto dist = m_Distances[i];
+
+				if (dist <= tol)
+					m_WithinTolPlanes.emplace_back(m_Planes[i]);
+
+				if (dist < closestDist)
+				{
+					closestDist = dist;
+					closest = m_Planes[i];
+				}
+			}
+
+			if (m_WithinTolPlanes.size() == 0u)
+				m_WithinTolPlanes.emplace_back(closest);
+		}
+
+		const auto& GetNearbyPlanes() const
+		{
+			return m_WithinTolPlanes;
+		}
+
+	private:
+		std::array<int, MaxNumPlanes> m_PlanesIds;
+		std::array<Plane, MaxNumPlanes> m_Planes;
+		std::array<float, MaxNumPlanes> m_Distances;
+
+		std::vector<Plane> m_WithinTolPlanes;
+	};
+
+	float DistanceFromPointToFace(const Vector3& p, const Face& f) const
+	{
+
+	}
+
 	void FindNearbyPlanes(const std::vector<Face*>& faces, const Vector3& p)
 	{
-		m_UsedPlanes.clear();
-		m_NearbyPlanes.clear();
+		m_NearbyPlanes.Reset();
 
 		for (auto f : faces)
 		{
-			// find the closest 3 planes (or less than 3 if none are closer than some tol)
+			auto dist = DistanceFromPointToFace(p, *f);
+			m_NearbyPlanes.Update(f->GetPlaneId(), f->ToPlane(), dist);
 		}
+		m_NearbyPlanes.FinishUpdating(0.1f);
 	}
 
 	Plane CalculateSplitPlane(const Vector3& p) const
 	{
-		switch (m_NearbyPlanes.size())
+		auto& nearbyPlanes = m_NearbyPlanes.GetNearbyPlanes();
+
+		switch (nearbyPlanes.size())
 		{
 		case 1:
 		{
-			return m_NearbyPlanes[0];
+			return nearbyPlanes[0];
 		}
 		case 2:
 		{
 			Ray inter;
-			if (!m_NearbyPlanes[0].Intersects(m_NearbyPlanes[1], inter))
-				return m_NearbyPlanes[0];
+			if (!nearbyPlanes[0].Intersects(nearbyPlanes[1], inter))
+				return nearbyPlanes[0];
 
 			auto p0 = inter.ClosestPointOnRay(p);
-			auto n = (m_NearbyPlanes[0].GetNormal() + m_NearbyPlanes[1].GetNormal()).Normalized();
+			auto n = (nearbyPlanes[0].GetNormal() + nearbyPlanes[1].GetNormal()).Normalized();
 
 			return Plane(n, p0);
 		}
 		case 3:
 		{
 			Vector3 inter;
-			if (!m_NearbyPlanes[0].Intersects(m_NearbyPlanes[1], m_NearbyPlanes[2], inter))
-				return m_NearbyPlanes[0];
+			if (!nearbyPlanes[0].Intersects(nearbyPlanes[1], nearbyPlanes[2], inter))
+				return nearbyPlanes[0];
 
-			return Plane((m_NearbyPlanes[0].GetNormal() + m_NearbyPlanes[1].GetNormal() + m_NearbyPlanes[2].GetNormal()).Normalized(),
+			return Plane((nearbyPlanes[0].GetNormal() + nearbyPlanes[1].GetNormal() + nearbyPlanes[2].GetNormal()).Normalized(),
 				inter);
 		}
 		default:
@@ -59,12 +157,9 @@ private:
 		}
 	}
 
-	Matrix4 CalculateCutShapesTransform(Transform& toSplitsTransform, const Plane& splitPlane, Quaternion& q) const
+	Matrix4 CalculateCutShapesTransform(Transform& toSplitsTransform, const Plane& splitPlane) const
 	{
-		auto v = (Vector3(1.0f, 1.0f, 1.0f));
-		auto vMag = v.Magnitude();
-
-		v /= vMag;
+		auto v = (Vector3(1.0f, 1.0f, 1.0f)).Normalized();
 		auto u = Vector3::OrthogonalDirection(v);
 		auto k = Vector3::Cross(v, u);
 
@@ -74,8 +169,8 @@ private:
 		R0.SetColumn(2, u.z, k.z, v.z, 0.0f);
 		R0.SetColumn(3, 0.0f, 0.0f, 0.0f, 1.0f);
 
-		auto sV = 5.0f;
-		auto S = Matrix4::FromScale(10.0f, 10.0f, sV / vMag);
+		auto scaleFactor = 10.0f;
+		auto S = Matrix4::FromScale(scaleFactor, scaleFactor, scaleFactor);
 
 		auto& n = splitPlane.GetNormal();
 		auto R1 = Matrix4::FromRotation(Quaternion::LookRotation(-n, Vector3::OrthogonalDirection(n))); // could have some randomnes for up
@@ -83,9 +178,10 @@ private:
 		auto& P = splitPlane.GetP0();
 		auto D = 0.25f * P.Magnitude(); // could have some randomnes
 
+		auto sV = (Vector3(scaleFactor, scaleFactor, scaleFactor)).Magnitude();
 		auto T = Matrix4::FromTranslation(P + (sV - D) * n);
 
-		return T * R1 * S * R0;
+		return T * R1 * R0 * S;
 	}
 
 	Matrix4 CalculateCutShapesTransform(Transform& toSplitsTransform, const Vector3& splitPoint, const Vector3& splitNormal, Quaternion& q) const
@@ -114,10 +210,9 @@ public:
 		FindNearbyPlanes(toSplit.GetFaces(), splitPoint);
 		auto splitPlane = CalculateSplitPlane(splitPoint);
 
-		Quaternion q;
-		auto M = CalculateCutShapesTransform(toSplitsTransform, splitPlane, q);
+		auto M = CalculateCutShapesTransform(toSplitsTransform, splitPlane);
 
-		m_FacesCreator.CreateFaces(m_CutShape, M, q, firstPlaneId);
+		// Add faces
 
 		m_CutShape.OnAllFacesAdded();
 
@@ -126,8 +221,6 @@ public:
 
 private:
 	Shape m_CutShape;
-	CubeFacesCreator m_FacesCreator;
 
-	std::vector<Plane> m_NearbyPlanes;
-	std::vector<int> m_UsedPlanes;
+	ClosestPlanes m_NearbyPlanes;
 };
